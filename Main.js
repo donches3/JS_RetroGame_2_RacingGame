f30a3126fcd311779a070003d3e63b74bfe6c066
Don Chesnut <donches3@arrayschool.com>
2017-07-12 13:16:46 -0600

    Rudimentary loading screen

--- Main.js
+++ Main.js
@@ -5,6 +5,12 @@ var canvas, canvasContext;
 window.onload = function() {
     canvas = document.getElementById('gameCanvas');
     canvasContext = canvas.getContext('2d');
+
+    // rudimentary loading screen
+    colorRect(0,0, canvas.width, canvas.height, 'black');
+    colorText("LOADING IMAGES", canvas.width/2, canvas.height/2, 'white');
+
+    // game won't start until images finish loading
     loadImages();
 } // end window.onload ---------------------------------------------------------
 
