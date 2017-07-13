74405adad9154b68d52d2ced36f1fe8370f906e6
Don Chesnut <donches3@arrayschool.com>
2017-07-13 10:59:11 -0600

    Car only turns while moving

--- js/Car.js
+++ js/Car.js
@@ -7,7 +7,8 @@ var carSpeed = 0;
 const SPEED_DECAY_MULT = 0.94;
 const DRIVE_POWER = 0.5;
 const REVERSE_POWER = 0.2;
-const TURN_RATE = 0.03;
+const TURN_RATE = 0.06;
+const MIN_SPEED_TO_TURN = 0.5;
 // end vars --------------------------------------------------------------------
 
 function carReset() {
@@ -33,12 +34,16 @@ function carMove() {
     if(keyHeld_Reverse) {
         carSpeed -= REVERSE_POWER;
     }
-    if(keyHeld_TurnLeft) {
-        carAng -= TURN_RATE;
+
+    if(Math.abs(carSpeed) > MIN_SPEED_TO_TURN) { // only turn while moving
+        if(keyHeld_TurnLeft) {
+            carAng -= TURN_RATE;
+        }
+        if(keyHeld_TurnRight) {
+            carAng += TURN_RATE;
+        }
     }
-    if(keyHeld_TurnRight) {
-        carAng += TURN_RATE;
-    }
+
     // increment car position
     carX += Math.cos(carAng) * carSpeed;
     carY += Math.sin(carAng) * carSpeed;
